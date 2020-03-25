ef29eea9abd1bdd23f34fb82d70efcc9ffb7b91c
minyeling <2024896559@qq.com>
2020-03-25 23:44:29 +0800

    一个springmvc 以后下载使用，不用在配置

---
+++ src/main/java/com/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("hello")
+    public @ResponseBody
+    String hello(){
+        return "Hello,World!我爱你，世界！";
+    }
+
+}
